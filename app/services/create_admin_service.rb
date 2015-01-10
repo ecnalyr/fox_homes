5082ee71c3c396b18aef1f985d6d39e0eb39d206
ecnalyr <ecnalyr@gmail.com>
2015-01-10 15:58:19 -0600

    Removed references to environment variables for heroku deploy.

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,8 +1,8 @@
 class CreateAdminService
   def call
-    user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
-        user.password = Rails.application.secrets.admin_password
-        user.password_confirmation = Rails.application.secrets.admin_password
+    user = User.find_or_create_by!(email: 'example@example.com') do |user|
+        user.password = 'pas5word'
+        user.password_confirmation = 'pas5word'
         user.admin!
       end
   end
